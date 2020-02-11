8c47ae190abb651d201491bacccf14d439102698
kmzn128 <54118781+kmzn128@users.noreply.github.com>
2020-02-11 11:06:02 -0800

    modified leet77

--- python/77.py
+++ python/77.py
@@ -1,18 +1,19 @@
-def nCk(n, k, v):
-    ans = []
-    if(k == 1):
-        for e in v:
-            ans.append([e])
-        return ans
+def nCk(n, k, v, cur, elem, ans):
+    if(len(elem) == k):
+        ans.append(elem.copy())
     else:
-        top = v.pop(0)
-        vv = nCk(n-1, k-1, v)
-        for e in vv:
-            ans.append([top]+e)
-        return ans
+        for i in range(cur, n):
+             elem.append(v[i])
+             nCk(n, k, v, i+1, elem, ans)
+             elem.pop()
+             
+
 
 
 n = 6
 k = 3
 v = [i for i in range(1, n+1)]
-print(nCk(n, k, v))
+elem = []
+ans = []
+nCk(n, k, v, 0, elem, ans)
+print(ans)
